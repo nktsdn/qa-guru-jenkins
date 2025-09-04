9b6a1b548467d2707d32abfb68ba9b922ec8c3e0
work <sadiulinn@bk.ru>
2025-09-04 13:08:14 +0500

    1 commit

---
+++ src/test/java/simple/NegativeTests.java
@@ -0,0 +1,29 @@
+package simple;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NegativeTests {
+
+    @Test
+    void test1(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test2(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test3(){
+        assertTrue(false);
+    }
+
+    @Test
+    void test4(){
+        assertTrue(false);
+    }
+
+}
